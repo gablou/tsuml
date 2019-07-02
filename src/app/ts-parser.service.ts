7ccb35a92a7208cb0a9e2f0a08214f330c14cd66
gablou <el.gablou@outlook.fr>
2019-07-02 23:30:46 +0200

    catch some error to allow partial parcing

--- src/app/ts-parser.service.ts
+++ src/app/ts-parser.service.ts
@@ -12,30 +12,39 @@ export class TsParserService {
     const className = input.match(/class (\w+) \{/)[1];
     const properties: UMLProperty[] = [];
     const methods: UMLmethod[] = [];
-    properties.push(...(input.match(/^ *private (\w+): (\w+);/gm).map(
-      line => {
-        const tmp = line.match(/ *private (\w+): (\w+);/);
-        return { name: tmp[1], type: tmp[2], visibility: 'private' as UMLVisibility, isClassifier: false };
-      }
-    )));
-    properties.push(...(input.match(/^ *protected (\w+): (\w+);/gm).map(
-      line => {
-        const tmp = line.match(/ *protected (\w+): (\w+);/);
-        return { name: tmp[1], type: tmp[2], visibility: 'protected' as UMLVisibility, isClassifier: false };
-      }
-    )));
-    methods.push(...(input.match(/^ *(public|private|protected|)( static | |)(\w+)\((.*)\)(: |)(\w+|) {/gm).map(
-      line => {
-        const tmp = line.match(/^ *(public|private|protected|)( static | |)(\w+)\((.*)\)(: |)(\w+|) {/);
-        return {
-          name: tmp[3],
-          parameters: tmp[4],
-          returnType: tmp[6],
-          visibility: tmp[1] === '' ? 'public' as UMLVisibility : tmp[1] as UMLVisibility,
-          isClassifier: tmp[2] === ' static '
-        };
-      }
-    )));
+    try {
+      properties.push(...(input.match(/^ *private (\w+): (\w+);/gm).map(
+        line => {
+          const tmp = line.match(/ *private (\w+): (\w+);/);
+          return { name: tmp[1], type: tmp[2], visibility: 'private' as UMLVisibility, isClassifier: false };
+        }
+      )));
+    } catch (error) {
+    }
+    try {
+      properties.push(...(input.match(/^ *protected (\w+): (\w+);/gm).map(
+        line => {
+          const tmp = line.match(/ *protected (\w+): (\w+);/);
+          return { name: tmp[1], type: tmp[2], visibility: 'protected' as UMLVisibility, isClassifier: false };
+        }
+      )));
+    } catch (error) {
+    }
+    try {
+      methods.push(...(input.match(/^ *(public|private|protected|)( static | |)(\w+)\((.*)\)(: |)(\w+|) {/gm).map(
+        line => {
+          const tmp = line.match(/^ *(public|private|protected|)( static | |)(\w+)\((.*)\)(: |)(\w+|) {/);
+          return {
+            name: tmp[3],
+            parameters: tmp[4],
+            returnType: tmp[6],
+            visibility: tmp[1] === '' ? 'public' as UMLVisibility : tmp[1] as UMLVisibility,
+            isClassifier: tmp[2] === ' static '
+          };
+        }
+      )));
+    } catch (error) {
+    }
     return {
       name: className,
       properties,
